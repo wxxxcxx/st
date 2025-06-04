bb90727d65ebc3f5ea64f5ed2a0c30a2ed1562d9
W <wxxxcxx@foxmail.com>
2025-06-04 17:45:05 +0800

    .

--- st/src/gummy.rs
+++ st/src/gummy.rs
@@ -1,8 +1,8 @@
+use anyhow::Ok;
+use async_trait::async_trait;
 use futures_util::stream::{SplitSink, SplitStream};
 use futures_util::{SinkExt, StreamExt};
 use serde::{Deserialize, Serialize};
-use std::result::Result::Ok;
-use tokio::task;
 use tokio_tungstenite::{WebSocketStream, connect_async_tls_with_config};
 use tungstenite::Message;
 use tungstenite::client::IntoClientRequest;
@@ -94,24 +94,151 @@ type WSWriter =
 type WSReader =
     SplitStream<WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>>;
 
-pub struct Initial {
+pub struct Closed {
     api_key: String,
 }
 
-pub struct ReadyForTask {
+pub struct Connected {
+    api_key: String,
     writer: WSWriter,
     reader: WSReader,
 }
 
-pub struct InTask {
+pub struct Transmitting {
+    api_key: String,
     writer: WSWriter,
     reader: WSReader,
     task_id: String,
 }
 
+#[async_trait]
+pub trait State {
+    async fn connect(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error>;
+    async fn start_task(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error>;
+    async fn send_data(self: Box<Self>, data: &[u8]) -> Result<Box<dyn State>, anyhow::Error>;
+    async fn finish_task(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error>;
+    async fn close(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error>;
+}
+
+#[async_trait]
+impl State for Closed {
+    async fn connect(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        let mut request =
+            "wss://dashscope.aliyuncs.com/api-ws/v1/inference".into_client_request()?;
+        request
+            .headers_mut()
+            .insert("Authorization", format!("Bearer {}", self.api_key).parse()?);
+        request.headers_mut().insert("user-agent", "app".parse()?);
+        request
+            .headers_mut()
+            .insert("X-DashScope-WorkSpace", "llm-hxfupix3oo63uw6d".parse()?);
+        request
+            .headers_mut()
+            .insert("X-DashScope-DataInspection", "enable".parse()?);
+        let (stream, _) = connect_async_tls_with_config(request, None, false, None).await?;
+        let (writer, reader) = stream.split();
+        Ok(Box::new(Connected {
+            api_key: self.api_key,
+            writer,
+            reader,
+        }))
+    }
+
+    async fn start_task(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        todo!()
+    }
+
+    async fn send_data(self: Box<Self>, data: &[u8]) -> Result<Box<dyn State>, anyhow::Error> {
+        todo!()
+    }
+
+    async fn finish_task(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        todo!()
+    }
+
+    async fn close(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        Ok(self)
+    }
+}
+
+#[async_trait]
+impl State for Connected {
+    async fn connect(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        Err(anyhow::anyhow!("Already connected"))
+    }
+
+    async fn start_task(mut self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        let task_id = uuid::Uuid::new_v4().to_string();
+        let message = RequestMessage::new("run-task", &task_id);
+        self.writer
+            .send(Message::Text(
+                serde_json::to_string(&message).unwrap().into(),
+            ))
+            .await?;
+        Ok(Box::new(Transmitting {
+            api_key: self.api_key,
+            writer: self.writer,
+            reader: self.reader,
+            task_id,
+        }))
+    }
+
+    async fn send_data(self: Box<Self>, data: &[u8]) -> Result<Box<dyn State>, anyhow::Error> {
+        Err(anyhow::anyhow!("Cannot send data in connected state"))
+    }
+
+    async fn finish_task(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        Err(anyhow::anyhow!("Cannot finish task in connected state"))
+    }
+
+    async fn close(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        Ok(Box::new(Closed {
+            api_key: self.api_key,
+        }))
+    }
+}
+
+#[async_trait]
+impl State for Transmitting {
+    async fn connect(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        Err(anyhow::anyhow!("Already in task"))
+    }
+
+    async fn start_task(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        Err(anyhow::anyhow!("Already in task"))
+    }
+
+    async fn send_data(mut self: Box<Self>, data: &[u8]) -> Result<Box<dyn State>, anyhow::Error> {
+        self.writer
+            .send(Message::Binary(data.to_vec().into()))
+            .await?;
+        Ok(self)
+    }
+
+    async fn finish_task(mut self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        let message = RequestMessage::new("finish-task", &self.task_id);
+        self.writer
+            .send(Message::Text(
+                serde_json::to_string(&message).unwrap().into(),
+            ))
+            .await?;
+        Ok(Box::new(Connected {
+            api_key: self.api_key,
+            writer: self.writer,
+            reader: self.reader,
+        }))
+    }
+
+    async fn close(self: Box<Self>) -> Result<Box<dyn State>, anyhow::Error> {
+        Ok(Box::new(Closed {
+            api_key: self.api_key,
+        }))
+    }
+}
+
 pub enum Gummy {
     ReadyForTask(ReadyForTask),
-    InTask(InTask),
+    InTask(Transmitting),
     Closed,
     Error(String),
 }
@@ -159,7 +286,7 @@ impl Gummy {
                         .ok_or_else(|| anyhow::anyhow!("Invalid response format"))?;
                     if event == "task-started" {
                         println!("Task started successfully.");
-                        return Ok(Gummy::InTask(InTask {
+                        return Ok(Gummy::InTask(Transmitting {
                             writer: ready.writer,
                             reader: ready.reader,
                             task_id: response["header"]["task_id"]
--- st/src/main.rs
+++ st/src/main.rs
@@ -2,6 +2,7 @@ use audio::recorder::{CpalRecorder, OutputFormat, Recorder, RecorderSampleFormat
 use audio::wav::Wav;
 use gummy::Gummy;
 use serde::{Deserialize, Serialize};
+use std::env::{var, vars};
 use std::sync::{Arc, Mutex};
 use std::thread::sleep;
 use std::time::Duration;
@@ -55,56 +56,56 @@ fn main() {
     let recorder_format = CpalRecorder::output_format();
     let (tx, rx) = channel();
     let output = audio::recorder::ChannelRecorderOutput { sender: tx };
-    let wav = Arc::new(Mutex::new(Some(Wav::new(
-        &wav_path,
-        &recorder_format,
-    ))));
+    let wav = Arc::new(Mutex::new(Some(Wav::new(&wav_path, &recorder_format))));
     let wav_cloned = Arc::clone(&wav);
 
     spawn(move || {
         let rt = Builder::new_current_thread().enable_all().build().unwrap();
-       
-        let gummy = rt.block_on(async {
-            Gummy::connect("api_key".to_string()).await.unwrap()
-        });
-        let mut buffer = Vec::new();
-        while let Ok(data) = rx.recv() {
-            println!(
-                "[{}] Received {} bytes of audio data",
-                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-                data.len(),
-            );
-
-            if let Ok(mut wav) = wav_cloned.lock() {
-                if let Some(wav) = wav.as_mut() {
-                    wav.write::<i16, i16>(&data)
-                        .expect("Failed to write to WAV");
+        rt.block_on(async {
+            let mut buffer = Vec::new();
+            let api_key = var("API_KEY").expect("API_KEY environment variable not set");
+            let mut gummy: Gummy = Gummy::connect(api_key).await.unwrap();
+            while let Ok(data) = rx.recv() {
+                println!(
+                    "[{}] Received {} bytes of audio data",
+                    chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                    data.len(),
+                );
+                if let Ok(mut wav) = wav_cloned.lock() {
+                    if let Some(wav) = wav.as_mut() {
+                        wav.write::<i16, i16>(&data)
+                            .expect("Failed to write to WAV");
+                    }
+                } else {
+                    eprintln!("Failed to lock WAV writer");
                 }
-            } else {
-                eprintln!("Failed to lock WAV writer");
+                buffer.extend_from_slice(&data);
+                let duration_per_sample: f32 = recorder_format.sample_rate as f32
+                    * (recorder_format.sample_format.sample_size()) as f32
+                    * recorder_format.channels as f32;
+                println!("Duration per sample: {} bytes", duration_per_sample);
+                let buffer_duration = buffer.len() as f32 / duration_per_sample;
+                println!("Buffer duration: {} s", buffer_duration);
+                if buffer_duration >= 0.1 {
+                    println!("Buffer duration exceeded 100ms, processing...");
+                    // Here you can process the buffer, e.g., send to Gummy
+                    gummy = gummy.start_task().await.unwrap();
+                    gummy
+                        .send_data(
+                            &buffer
+                                .iter()
+                                .map(|s| s.to_le_bytes())
+                                .flatten()
+                                .collect::<Vec<u8>>(),
+                        )
+                        .await
+                        .unwrap();
+                    gummy.finish_task().await.unwrap();
+                    buffer.clear();
+                }
             }
-            buffer.extend_from_slice(&data);
-            let duration_per_sample: f32 = recorder_format.sample_rate as f32
-                * (recorder_format.sample_format.sample_size()) as f32
-                * recorder_format.channels as f32;
-            println!("Duration per sample: {} bytes", duration_per_sample);
-            let buffer_duration = buffer.len() as f32 / duration_per_sample;
-            println!("Buffer duration: {} s", buffer_duration);
-            if buffer_duration >= 0.1 {
-                println!("Buffer duration exceeded 100ms, processing...");
-                // Here you can process the buffer, e.g., send to Gummy
-                rt.block_on(async {
-              
-                    // gummy.send_data(&buffer.iter().map(|s|s.to_le_bytes()).flatten().collect::<Vec<u8>>()).await.unwrap();
-                    // gummy.finish_task().await.unwrap();
-                    println!("Processing buffer with {} samples", buffer.len());
-                });
-                buffer.clear(); // Clear the buffer after processing
-            }
-        }
+        });
     });
-
-    
 
     recorder.start(output).expect("Failed to start recorder");
     println!("Recorder started, waiting for audio data...");
